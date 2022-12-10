de5e8b90cc2316f89a4e2bfa0d9bef0adb67e198
yuoku <appledev.yuoku@gmail.com>
2022-12-10 18:03:42 +0900

    Initial Commit

---
+++ StretchyHeader/StretchyHeaderApp.swift
@@ -0,0 +1,17 @@
+//
+//  StretchyHeaderApp.swift
+//  StretchyHeader
+//
+//  Created by Yuki Okudera on 2022/12/10.
+//
+
+import SwiftUI
+
+@main
+struct StretchyHeaderApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ StretchyHeaderUITests/StretchyHeaderUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  StretchyHeaderUITestsLaunchTests.swift
+//  StretchyHeaderUITests
+//
+//  Created by Yuki Okudera on 2022/12/10.
+//
+
+import XCTest
+
+final class StretchyHeaderUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
